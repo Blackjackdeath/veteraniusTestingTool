fa727fa5b22c1f555dbf9983d23da8cbdbbec8cf
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2022-01-02 19:38:10 +0200

    add type answer

--- script/script.js
+++ script/script.js
@@ -1,30 +1,51 @@
 let modelObj={
     qestion:'',
-    answers:[{
+    type:'',
+    answers:[]
+};
+
+function answerArray(selector){
+    let arr=[];
+    let obj={
         answer:'',
         right:false
-    },{
-        answer:'',
-        right:false
-    },{
-        answer:'',
-        right:false
-    },{
-        answer:'',
-        right:false
-    }]
+    };
+    for(let i=0; i<document.querySelectorAll(selector).length;i++){
+       obj.answer=document.querySelectorAll(selector)[i].value;
+       obj.right=document.querySelectorAll(selector)[i].nextElemetSibling.checked;
+       arr.push(Object.assign({},obj))
+    };
+    return arr;
 };
 
 function send(){
     modelObj.qestion=document.getElementsByTagName('textarea')[0].value;
-    modelObj.answers[0].answer=document.getElementById('answer1').value;
-    modelObj.answers[0].right=document.getElementById('answer1').nextElementSibling.nextElementSibling.checked;
-    modelObj.answers[1].answer=document.getElementById('answer2').value;
-    modelObj.answers[1].right=document.getElementById('answer2').nextElementSibling.nextElementSibling.checked;
-    modelObj.answers[2].answer=document.getElementById('answer3').value;
-    modelObj.answers[2].right=document.getElementById('answer3').nextElementSibling.nextElementSibling.checked;
-    modelObj.answers[3].answer=document.getElementById('answer4').value;
-    modelObj.answers[3].right=document.getElementById('answer4').nextElementSibling.nextElementSibling.checked;
+    while (true) {
+        let i=0;
+        if (document.querySelectorAll('.adminBoxTypeAnswerBoxAnswer__input')[i].checked){
+            modelObj.type=document.querySelectorAll('.adminBoxTypeAnswerBoxAnswer__input')[i].value;
+            break;
+        }
+        else{
+            i++;
+        };    
+    };
+    switch (modelObj.type) {
+        case 'one':
+            modelObj.answers=answerArray('.oneChooseAnswer__text');
+            break;
+        case 'many':
+            modelObj.answers=answerArray('.manyChooseAnswer__text');
+            break;
+        case 'freedom':
+            modelObj.answers.push(undefined);
+            break;
+        case 'missword':
+            modelObj.answers=answerArray('.oneChooseAnswer__text');
+            break;
+        default:
+            break;
+    };
     console.log(modelObj);
     // async function sendPost(){
     //     try {
@@ -42,4 +63,46 @@ function send(){
     //         console.log(error)
     //     }
     // }
+};
+
+function login(){
+    if ((document.getElementById('password').value==='1')&&(document.getElementById('login').value==='Admin')){
+        document.querySelector('.modalWindow').style.display="none";
+    }
+    else{
+        alert('Error');
+    }
+};
+
+function choose(event){
+    if (event.target.tagName==="INPUT"){
+        switch (event.target.value) {
+            case 'one':
+                document.querySelector('.oneChoose__description').textContent='Введіть варіанти відповідів і виберіть одну правильнух';
+                document.querySelector('.oneChoose').style.display="block";
+                document.querySelector('.manyChoose').style.display="none";
+                break;
+            case 'many':
+                document.querySelector('.oneChoose').style.display="none";
+                document.querySelector('.manyChoose').style.display="block";
+                break;
+            case 'freedom':
+                document.querySelector('.oneChoose').style.display="none";
+                document.querySelector('.manyChoose').style.display="none";
+                break;
+            case 'missword':
+                document.querySelector('.oneChoose__description').textContent='Введіть варіанти відповідів і виберіть одну правильну. В тексті питання поставте {var} де повинен бути пропуск (не забувайте за пробіли)';
+                document.querySelector('.oneChoose').style.display="block";
+                document.querySelector('.manyChoose').style.display="none";
+                break;
+            default:
+                break;
+        }
+    }
 }
+
+function init(){
+    document.querySelector('.accept').addEventListener('click', login);
+    document.querySelector('.adminBoxTypeAnswerBox').addEventListener('click', ()=>choose(event));
+};
+init();
