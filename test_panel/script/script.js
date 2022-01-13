03eb0f183cc13308e688bf5338037d4a64e51ef9
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2022-01-13 16:03:30 +0200

    test work 1,2 v. q.

--- test_panel/script/script.js
+++ test_panel/script/script.js
@@ -1,11 +1,14 @@
 let emailRegExp = /^[\w_\-\.]+@\w+\.\w+$/;
 let userRegexp = /^[\w]+\s+[\w]+$/;
-function statr() {
+let test = {};
+let count = 1;
+function start() {
     let obj = {};
-    obj["first_name"] = document.querySelectorAll('.mainLoginBox__input')[0].value;
+    let arrNameUser = [];
+    arrNameUser = document.querySelectorAll('.mainLoginBox__input')[0].value.split(' ');
+    obj["first_name"] = arrNameUser[0];
     obj["email"] = document.querySelectorAll('.mainLoginBox__input')[1].value;
-    obj["last_name"] = "Hapak";
-    console.log(obj);
+    obj["last_name"] = arrNameUser[1];
     request(obj);
 };
 
@@ -23,12 +26,19 @@ function checkInfoUser() {
         document.querySelector('.mainLoginBoxLoginError').style.display = 'block';
     }
     if ((testEmail) && (testName)) {
-        statr();
+        start();
     };
 };
 
 function init() {
     document.querySelector('.mainLogin__button').addEventListener('click', checkInfoUser);
+    document.querySelector('.next').addEventListener('click', () => {
+        count++;
+        if (count === test.length) {
+            document.querySelector('.next').disabled = true;
+        }
+        render(count, test);
+    });
 };
 
 async function request(user) {
@@ -40,13 +50,48 @@ async function request(user) {
             },
             body: JSON.stringify(user)
         });
-        const request = await response.json();
-        console.log(request);
-        document.querySelector('.mainInfo').style.display='none';
-        document.querySelector('.mainTestBox').style.display='block';
-
+        test = await response.json();
+        console.log(test);
+        document.querySelector('.mainInfo').style.display = 'none';
+        document.querySelector('.mainTestBox').style.display = 'block';
+        render(1, test);
     } catch (error) {
-         console.log(error);
+        console.log(error);
+    };
+};
+
+function timer() {
+    //Work;
+};
+
+function render(count, test) {
+    if (test[count].question_image === null) {
+        document.querySelector('.mainTestBoxQuestion__img').style.display = 'none';
+    } else {
+        document.querySelector('.mainTestBoxQuestion__img').style.display = 'block';
+        console.log(test[count].question_image);
+    };
+    document.querySelector('.mainTestBoxQuestion__text').textContent = test[count].question_text;
+    switch (test[count].question_type) {
+        case 1:
+            document.querySelector('.mainTestBoxAnswer').innerHTML = '<div><p>Введіть відповідь</p><textarea class="answer__textArea"></textarea></div>'
+            break;
+        case 2:
+            document.querySelector('.mainTestBoxAnswer').innerHTML='';
+            for (const key in test[count].answer_variants) {
+                document.querySelector('.mainTestBoxAnswer').innerHTML+=`<div class="mainTestBoxAnswerBox"><input type="radio" name="radio" id="" class="mainTestBoxAnswerBox__input"><span class="mainTestBoxAnswerBox__span">${test[count].answer_variants[key]}</span></div>`
+            };
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+
+        default:
+            break;
     }
-}
+};
+
 init();
