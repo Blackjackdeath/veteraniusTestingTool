3f777876d112842f27f26e5e2d7d1facf2725e47
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2022-01-13 11:45:38 +0200

    done gtml and css

--- test_panel/script/script.js
+++ test_panel/script/script.js
@@ -1,33 +1,52 @@
 let emailRegExp = /^[\w_\-\.]+@\w+\.\w+$/;
-
+let userRegexp = /^[\w]+\s+[\w]+$/;
 function statr() {
-    alert('Start');
-    let obj={};
-    obj["name"]=document.querySelectorAll('.mainLoginBox__input')[0].value;
-    obj["email"]=document.querySelectorAll('.mainLoginBox__input')[1].value;
+    let obj = {};
+    obj["first_name"] = document.querySelectorAll('.mainLoginBox__input')[0].value;
+    obj["email"] = document.querySelectorAll('.mainLoginBox__input')[1].value;
+    obj["last_name"] = "Hapak";
     console.log(obj);
+    request(obj);
 };
 
 function checkInfoUser() {
-    let test = emailRegExp.test(document.querySelectorAll('.mainLoginBox__input')[1].value);
-    if (test) {
-        document.querySelector('.mainLoginBoxEmailError').style.display='none';
-    }else{
-        document.querySelector('.mainLoginBoxEmailError').style.display='block';
+    let testEmail = emailRegExp.test(document.querySelectorAll('.mainLoginBox__input')[1].value);
+    let testName = userRegexp.test(document.querySelectorAll('.mainLoginBox__input')[0].value)
+    if (testEmail) {
+        document.querySelector('.mainLoginBoxEmailError').style.display = 'none';
+    } else {
+        document.querySelector('.mainLoginBoxEmailError').style.display = 'block';
     };
-    if (document.querySelectorAll('.mainLoginBox__input')[0].value!==''){
-        document.querySelector('.mainLoginBoxLoginError').style.display='none';
-    }else{
-        document.querySelector('.mainLoginBoxLoginError').style.display='block';
+    if (testName) {
+        document.querySelector('.mainLoginBoxLoginError').style.display = 'none';
+    } else {
+        document.querySelector('.mainLoginBoxLoginError').style.display = 'block';
     }
-    if ((test)&&(document.querySelectorAll('.mainLoginBox__input')[0].value!=='')) {
-        statr(); 
-    } ;
+    if ((testEmail) && (testName)) {
+        statr();
+    };
 };
 
 function init() {
     document.querySelector('.mainLogin__button').addEventListener('click', checkInfoUser);
-
 };
 
+async function request(user) {
+    try {
+        const response = await fetch('https://alexdko.pythonanywhere.com/testing/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        const request = await response.json();
+        console.log(request);
+        document.querySelector('.mainInfo').style.display='none';
+        document.querySelector('.mainTestBox').style.display='block';
+
+    } catch (error) {
+         console.log(error);
+    }
+}
 init();
