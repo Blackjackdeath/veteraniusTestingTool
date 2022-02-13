7601415394e489b9dcf0832bfbfea10120dfd4d4
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2022-02-13 14:24:31 +0200

    remake v3

--- test_panel/script/script.js
+++ test_panel/script/script.js
@@ -3,9 +3,9 @@ let userRegexp = /^[аАбБвВгГдДеЕжЖзЗиИіІїЇйЙкКлЛмМ
 let test = {};
 let count = 1;
 let time = 0;
-let timeQuestion=0;
-let answerTestObj=[];
-let result={};
+let timeQuestion = 0;
+let answerTestObj = [];
+let result = {};
 
 //Функція старт
 function start() {
@@ -30,36 +30,43 @@ function checkInfoUser() {
         document.querySelector('.mainLoginBoxLoginError').style.display = 'none';
     } else {
         document.querySelector('.mainLoginBoxLoginError').style.display = 'block';
-    }
-    if ((testEmail) && (testName)) {
+    };
+    if (document.getElementById('permission').checked) {
+        document.querySelector('.mainLoginBoxPermissionError').style.display = 'none';
+    } else {
+        document.querySelector('.mainLoginBoxPermissionError').style.display = 'block';
+    };
+    if ((testEmail) && (testName) && (document.getElementById('permission').checked)) {
         document.querySelector('.mainLogin__button').disabled = true;
         start();
     };
 };
 // Функція ініціалізації привязка евентів до кнопок
 function init() {
-    document.querySelector('.back').disabled = true;
     document.querySelector('.mainLogin__button').addEventListener('click', checkInfoUser);
+    document.querySelector('.modalWindowMessageBox__button').addEventListener('click',()=>{
+        document.querySelector('.modalWindow').style.display = 'none';
+    })
     document.querySelector('.next').addEventListener('click', () => {
-        if (document.querySelector('.back').disabled === true) {
-            document.querySelector('.back').disabled = false;
+        if (document.querySelector('.back').style.display = 'none') {
+            document.querySelector('.back').style.display = 'block';
         };
         rememberAnswer(test[count]);
-        test[count]['answer_time']=Date.now()-timeQuestion;
+        test[count]['answer_time'] = Math.trunc((Date.now() - timeQuestion) / 1000);
         count++;
         if (count === test.length - 1) {
-            document.querySelector('.next').disabled = true;
+            document.querySelector('.next').style.display = 'none';
         };
         render(test[count]);
     });
     document.querySelector('.back').addEventListener('click', () => {
-        if (document.querySelector('.next').disabled === true) {
-            document.querySelector('.next').disabled = false;
+        if (document.querySelector('.next').style.display = 'none') {
+            document.querySelector('.next').style.display = 'block';
         };
         rememberAnswer(test[count]);
         count--;
         if (count === 1) {
-            document.querySelector('.back').disabled = true;
+            document.querySelector('.back').style.display = 'none';
         };
         render(test[count]);
     });
@@ -77,12 +84,18 @@ async function request(user) {
         });
         test = await response.json();
         console.log(test);
-        document.querySelector('.mainInfo').style.display = 'none';
-        document.querySelector('.mainTestBox').style.display = 'block';
-        let m = test[0].time_for_test;
-        time = parseInt(m) * 60;
-        render(test[1]);
-        clock = setInterval(timer, 1000);
+        if (test.user_id === -1) {
+            document.querySelector('.modalWindow').style.display = 'block';
+            document.querySelector('.mainLogin__button').disabled = false;
+        } else {
+            document.querySelector('.mainInfo').style.display = 'none';
+            document.querySelector('.mainTestBox').style.display = 'block';
+            let m = test[0].time_for_test;
+            time = parseInt(m) * 60;
+            render(test[1]);
+            clock = setInterval(timer, 1000);
+        }
+
     } catch (error) {
         console.log(error);
     };
@@ -102,6 +115,7 @@ function timer() {
     if (time == 0) {
         showResult();
     }
+
 };
 //Функція показу і навігації по питаннях тесту
 function render(obj) {
@@ -112,11 +126,12 @@ function render(obj) {
         document.querySelector('.mainTestBoxQuestion__img').setAttribute('src', obj.question_image);
     };
     document.querySelector('.mainTestBoxQuestion__text').textContent = test[count].question_text;
+    document.querySelector('.mainHeader__askCount').textContent=`Кількість питань:${count}/${test.length-1}`
     switch (obj.question_type) {
         case 1:
             document.querySelector('.mainTestBoxAnswer').innerHTML = '<div><p>Введіть відповідь</p><textarea class="answer__textArea"></textarea></div>';
-            if ('answer' in obj){
-                document.querySelector('.answer__textArea').value=obj['answer'];
+            if ('answer' in obj) {
+                document.querySelector('.answer__textArea').value = obj['answer'];
             };
             break;
         case 2:
@@ -124,11 +139,11 @@ function render(obj) {
             for (const key in obj.answer_variants) {
                 document.querySelector('.mainTestBoxAnswer').innerHTML += `<div class="mainTestBoxAnswerBox"><input type="radio" name="radio" id="" class="mainTestBoxAnswerBox__input"><span class="mainTestBoxAnswerBox__span">${test[count].answer_variants[key]}</span></div>`
             };
-            if ('answer' in obj){
+            if ('answer' in obj) {
                 for (let i = 0; i < document.querySelectorAll('.mainTestBoxAnswerBox__span').length; i++) {
-                    if (document.querySelectorAll('.mainTestBoxAnswerBox__span')[i].textContent===obj['answer']) {
-                        document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked=true;
-                    };    
+                    if (document.querySelectorAll('.mainTestBoxAnswerBox__span')[i].textContent === obj['answer']) {
+                        document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked = true;
+                    };
                 };
             };
             break;
@@ -137,13 +152,13 @@ function render(obj) {
             for (const key in obj.answer_variants) {
                 document.querySelector('.mainTestBoxAnswer').innerHTML += `<div class="mainTestBoxAnswerBox"><input type="checkbox" name="" id="" class="mainTestBoxAnswerBox__input"><span class="mainTestBoxAnswerBox__span">${test[count].answer_variants[key]}</span></div>`
             };
-            if ('answer' in obj){
+            if ('answer' in obj) {
                 for (let i = 0; i < document.querySelectorAll('.mainTestBoxAnswerBox__span').length; i++) {
-                    for (key in obj['answer']){
-                        if (document.querySelectorAll('.mainTestBoxAnswerBox__span')[i].textContent===obj['answer'][key]) {
-                            document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked=true;
-                        };    
-                    };  
+                    for (key in obj['answer']) {
+                        if (document.querySelectorAll('.mainTestBoxAnswerBox__span')[i].textContent === obj['answer'][key]) {
+                            document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked = true;
+                        };
+                    };
                 };
             };
             break;
@@ -154,7 +169,7 @@ function render(obj) {
         default:
             break;
     };
-    timeQuestion=Date.now();
+    timeQuestion = Date.now();
 };
 //Функці запису відповіді.
 function rememberAnswer(obj) {
@@ -166,14 +181,14 @@ function rememberAnswer(obj) {
             for (let i = 0; i < document.querySelectorAll('.mainTestBoxAnswerBox__input').length; i++) {
                 if (document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked) {
                     obj['answer'] = document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].nextElementSibling.textContent;
-                };    
+                };
             };
             break;
         case 3:
-            obj['answer']={};
+            obj['answer'] = {};
             for (let i = 0; i < document.querySelectorAll('.mainTestBoxAnswerBox__input').length; i++) {
                 if (document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].checked) {
-                    obj['answer'][`correct_answer${i+1}`] = document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].nextElementSibling.textContent;
+                    obj['answer'][`correct_answer${i + 1}`] = document.querySelectorAll('.mainTestBoxAnswerBox__input')[i].nextElementSibling.textContent;
                 };
             };
             break;
@@ -186,10 +201,10 @@ function rememberAnswer(obj) {
     };
 };
 //Функція провірки чи є відповідь уже в обєкті
-function showResult(){
+function showResult() {
     clearInterval(clock);
-    rememberAnswer(test[test.length-1]);
-    test[test.length-1]['answer_time']=Math.trunc((Date.now()-timeQuestion)/1000);
+    rememberAnswer(test[test.length - 1]);
+    test[test.length - 1]['answer_time'] = Math.trunc((Date.now() - timeQuestion) / 1000);
     remakeObj();
     document.querySelector('.mainTestBox').style.display = 'none';
     document.querySelector('.mainEndTest').style.display = 'block';
@@ -197,7 +212,7 @@ function showResult(){
     console.log(answerTestObj);
 };
 //Функція відправки результату
-async function sendResult(){
+async function sendResult() {
     try {
         const response = await fetch('https://alexdko.pythonanywhere.com/answer/ ', {
             method: 'POST',
@@ -209,32 +224,32 @@ async function sendResult(){
         console.log(response);
         result = await response.json();
         console.log(result);
-        document.querySelector('.userId').textContent+=result.user_id;
-        document.querySelector('.points').textContent+=result.points;
-        document.querySelector('.percent').textContent+=result.percent;
+        document.querySelector('.userId').textContent += result.user_id;
+        document.querySelector('.points').textContent += result.points;
+        document.querySelector('.percent').textContent += result.percent;
     } catch (error) {
         console.log(error);
     };
 };
 //Функція підготовки обєкту до відправки
-function remakeObj(){
-    for (let i=1; i<test.length; i++){
-        let obj={};
-        obj['user_id']=test[0].user_id;
-        obj['question_id']=test[i].id;
-        if (test[i].answer===undefined){
-            obj['answer']="";
-        }else{
-            obj['answer']=test[i].answer;
+function remakeObj() {
+    for (let i = 1; i < test.length; i++) {
+        let obj = {};
+        obj['user_id'] = test[0].user_id;
+        obj['question_id'] = test[i].id;
+        if (test[i].answer === undefined) {
+            obj['answer'] = "";
+        } else {
+            obj['answer'] = test[i].answer;
         };
-        if (test[i].answer_time===undefined){
-            obj['answer_time']=0;
-        }else{
-            obj['answer_time']=test[i].answer_time;
+        if (test[i].answer_time === undefined) {
+            obj['answer_time'] = 0;
+        } else {
+            obj['answer_time'] = test[i].answer_time;
         };
         answerTestObj.push(obj);
     };
-    answerTestObj=JSON.stringify(answerTestObj);
+    answerTestObj = JSON.stringify(answerTestObj);
 }
 //початок роботи
 init();
